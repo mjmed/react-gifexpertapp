10b05de68449ef1a136df878b8c197d200316698
Maria Jose Medina <mjmedina.c719@gmail.com>
2020-09-22 18:47:26 -0300

    docs agregado

---
+++ docs/precache-manifest.428252e678f3561cce2c103a8831ee21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd96375eab8aa9b86289a5075068d775",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8a86aa8fbd768b18cc5",
+    "url": "/static/css/main.b8055880.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8a86aa8fbd768b18cc5",
+    "url": "/static/js/main.dea1ff9f.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
